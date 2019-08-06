e0dc62b0b13cbeabfccbba08989560cf834be3c1
clevernyyyy <adam@scha.al>
2019-08-06 03:29:44 -0500

    creating GH pages build

---
+++ docs/build/precache-manifest.98ffe03e1f020237335ea74c915abc1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18e57c0b8896ef96d6d3064bf84b984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6de333138b062332ab1b",
+    "url": "/static/css/2.194e2ed7.chunk.css"
+  },
+  {
+    "revision": "22cc720908ba517161c9",
+    "url": "/static/css/main.a545c384.chunk.css"
+  },
+  {
+    "revision": "6de333138b062332ab1b",
+    "url": "/static/js/2.da2fd8e8.chunk.js"
+  },
+  {
+    "revision": "22cc720908ba517161c9",
+    "url": "/static/js/main.839eadbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8aa6a37d91f26ccd6b4ef32e0ddca2ad",
+    "url": "/static/media/k_black.8aa6a37d.png"
+  },
+  {
+    "revision": "9b6b0b3532fa6fbba6ef0519d2271f64",
+    "url": "/static/media/kernelcon_black.9b6b0b35.png"
+  },
+  {
+    "revision": "794063645273866ede0800fd0f76eb01",
+    "url": "/static/media/kernelcon_vision.79406364.png"
+  },
+  {
+    "revision": "c552b7f722aa8c54aadf843b02b8604d",
+    "url": "/static/media/kernelcon_white.c552b7f7.png"
+  }
+]);
