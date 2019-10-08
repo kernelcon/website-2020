429675b2e519197c8043409d5b93b0e5a0282f83
Tim Vidas <tvidas@alumni.cmu.edu>
2019-10-08 16:24:27 -0500

    adds 2020 CFP supporting files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -452,6 +452,14 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+           {
+              test: /\.(pdf|md|txt|docx|odt|tex)$/,
+              include: paths.appSrc,
+              loader: require.resolve('file-loader'),
+              options: {
+                name: 'cfp/[name].[ext]',
+              },
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
